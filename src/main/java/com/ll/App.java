0beefce6283609f476b30b63838ab20020dd0cee
jasonoh22 <jasonoh22@naver.com>
2023-11-06 13:38:49 +0900

    feat : 리스트ë¼ 통해 목록 불러오기

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,10 +1,13 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner scanner;
     private int contentId;
+    private List<Quotation> Quotations;
     App(Scanner scanner){
         this.scanner = scanner;
         contentId = 0;
@@ -22,7 +25,17 @@ public class App {
                 String authorName = scanner.nextLine();
                 contentId++;
                 Quotation quotation = new Quotation(contentId,content,authorName);
+                Quotations = new ArrayList<>();
+                Quotations.add(quotation);
                 System.out.println(quotation.getId()+"번 명언이 등록되었습니다.");
+            }
+            else if(cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(int i=Quotations.size()-1;i>=0;i--){
+                    Quotation quotation = Quotations.get(i);
+                    System.out.println(quotation.getId()+" / "+quotation.getAuthorName()+" / "+ quotation.getContent());
+                }
             }
             else{
                 break;
