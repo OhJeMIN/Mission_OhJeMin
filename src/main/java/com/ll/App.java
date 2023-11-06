14a742e17d4df487494db7cda27b05ebc381da71
jasonoh22 <jasonoh22@naver.com>
2023-11-06 10:47:04 +0900

    2단계 완

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,9 +1,28 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
+    private Scanner scanner;
+    App(Scanner scanner){
+        this.scanner = scanner;
+    }
     void run(){
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
+
+        while(true){
+            System.out.println("명령) ");
+            String cmd = scanner.nextLine();
+            if(cmd.equals("등록")){
+                System.out.println("명언 : ");
+                String content = scanner.nextLine();
+                System.out.println("작가 : ");
+                String authorName = scanner.nextLine();
+            }
+            else{
+                break;
+            }
+        }
 
     }
 }
--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -12,7 +12,20 @@ public class AppTest {
         Scanner scanner = TestUtil.genScanner("""
                 종료
                 """.stripIndent());
-        new App().run();
+        new App(scanner).run();
+        scanner.close();
+    }
+
+    @Test
+    @DisplayName("등록 입력 시 명언 및 작가를 입력창이 뜬다.")
+    void inputEnroll(){
+        Scanner scanner = TestUtil.genScanner("""
+                등록
+                늦었다고 생각했을 때가 제일 늦었다.
+                박명수
+                종료
+                """.stripIndent());
+        new App(scanner).run();
         scanner.close();
     }
 }
