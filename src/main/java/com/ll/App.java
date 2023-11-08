e502948470d6ec084f6dca1b83092f7f78ce6b49
jasonoh22 <jasonoh22@naver.com>
2023-11-08 11:43:17 +0900

    actionLIst to stream()

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,18 +8,20 @@ public class App {
     private Scanner scanner;
     private int contentId;
     private List<Quotation> Quotations;
-    App(Scanner scanner){
+
+    App(Scanner scanner) {
         this.scanner = scanner;
         contentId = 0;
     }
-    void run(){
+
+    void run() {
         System.out.println("== 명언 앱 ==");
         Quotations = new ArrayList<>();
-        while(true){
+        while (true) {
             System.out.println("명령) ");
             String cmd = scanner.nextLine();
             Rq rq = new Rq(cmd);
-            switch (rq.getCmd()){
+            switch (rq.getCmd()) {
                 case "종료":
                     return;
                 case "등록":
@@ -39,15 +41,15 @@ public class App {
     }
 
     private void actionModify(Rq rq) {
-        int id = rq.getParamAsInt("id",0);
-        if(id == 0){
+        int id = rq.getParamAsInt("id", 0);
+        if (id == 0) {
             System.out.println("id를 입력해주세요.");
             return;
         }
-        int num = verifyId(id,-1);
+        int num = verifyId(id, -1);
 
-        if(num == -1){
-            System.out.println(id+ "번 명언은 존재하지 않습니다.");
+        if (num == -1) {
+            System.out.println(id + "번 명언은 존재하지 않습니다.");
             return;
         }
         Quotation quotation = Quotations.get(num);
@@ -64,26 +66,23 @@ public class App {
     }
 
     private void actionRemove(Rq rq) {
-        int id = rq.getParamAsInt("id",0);
-        if(id == 0){
+        int id = rq.getParamAsInt("id", 0);
+        if (id == 0) {
             System.out.println("id를 입력해주세요.");
         }
-        int num = verifyId(id,-1);
-        if(num == -1){
-            System.out.println(id+ "번 명언은 존재하지 않습니다.");
+        int num = verifyId(id, -1);
+        if (num == -1) {
+            System.out.println(id + "번 명언은 존재하지 않습니다.");
             return;
         }
         Quotations.remove(num);
-        System.out.println(id +"번 명언이 삭제되었습니다.");
+        System.out.println(id + "번 명언이 삭제되었습니다.");
     }
 
     private void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
-        for(int i=Quotations.size()-1;i>=0;i--){
-            Quotation quotation = Quotations.get(i);
-            System.out.println(quotation.getId()+" / "+quotation.getAuthorName()+" / "+ quotation.getContent());
-        }
+        Quotations.reversed().forEach(quotation -> System.out.println(quotation.getId() + " / " + quotation.getAuthorName() + " / " + quotation.getContent()));
     }
 
     private void actionRegister() {
@@ -92,15 +91,15 @@ public class App {
         System.out.println("작가 : ");
         String authorName = scanner.nextLine();
         contentId++;
-        Quotation quotation = new Quotation(contentId,content,authorName);
+        Quotation quotation = new Quotation(contentId, content, authorName);
         Quotations.add(quotation);
-        System.out.println(quotation.getId()+"번 명언이 등록되었습니다.");
+        System.out.println(quotation.getId() + "번 명언이 등록되었습니다.");
     }
 
-    int verifyId(int id, int defaultValue){
-        for(int i=0;i<Quotations.size();i++){
+    int verifyId(int id, int defaultValue) {
+        for (int i = 0; i < Quotations.size(); i++) {
             Quotation quotation = Quotations.get(i);
-            if(quotation.getId() == id){
+            if (quotation.getId() == id) {
                 return i;
             }
         }
